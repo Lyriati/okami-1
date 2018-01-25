d0857c907601d26ce7935d6bc0bf8ab6ba895515
Bowan <senkan.re.kyuu@gmail.com>
2018-01-25 16:48:57 -0700

    Update generic.js

--- tables/generic.js
+++ tables/generic.js
@@ -92,7 +92,7 @@ bingoList[13] = [
 bingoList[14] = [
  { name: "Obtain Fountain", types: ["Brush"] },
  { name: "Obtain 50 Demon Fangs", types: ["demonfangs"] },
- { name: "Fish a Marlin", types: ["fishing"] }
+ { name: "Fish a Marlin", types: ["fishing"] },
  { name: "Bloom all the clovers in Sei\'an City' ", types: ["clover"] },
 ];
 bingoList[15] = [
